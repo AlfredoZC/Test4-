ef3e51aca035bf8b6014d2f93375faf572a6aac6
AlfredoZC <josealfredo.zambranacruz.az@gmail.com>
2024-05-28 15:41:05 -0400

    Funcion Calcular energia cinetica implementada correctamente

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,9 +6,10 @@ Console.WriteLine("Hello, World!");
 bool checker = true;
 
 while(checker){
+
     Console.WriteLine("Que desea calcular?\n");
-    Console.WriteLine("1.- Fuerza dado masa y aceleracio\n");
-    Console.WriteLine("2.- Calcular el trabajo dado la fuerza y distancia\n");
+    Console.WriteLine("1.- Fuerza dado masa y aceleracion\n");
+    Console.WriteLine("2.- Calcular el \"trabajo\" dado la fuerza y distancia\n");
     Console.WriteLine("3.- Calcular la energia cinetica dado la masa y velocidad\n");
     Console.WriteLine("\n");
     Console.WriteLine("Ingrese su opcion del 1 - 3 : ");
@@ -16,8 +17,8 @@ while(checker){
 
 
     if(result == 1){
-        bool iterador = true;
-        while(iterador){
+        bool iterador1 = true;
+        while(iterador1){
             Console.WriteLine("Vamos a calcular la Fuerza!");
             Console.WriteLine("Ingrese la masa:");
             double masa = double.Parse(Console.ReadLine());
@@ -32,7 +33,7 @@ while(checker){
             if(checker1 == 1){
                 continue;
             }else{
-                iterador = false;
+                iterador1 = false;
             }
         }
         
@@ -40,34 +41,12 @@ while(checker){
 
     }else if( result == 2){
 
-        bool iterador = true;
-        while(iterador){
+        bool iterador2 = true;
+        while(iterador2){
             Console.WriteLine("Vamos a calcular el trabajo!");
             Console.WriteLine("Ingrese la fuerza:");
             double fuerza = double.Parse(Console.ReadLine());
-            Console.WriteLine("Ingres la distancia: ");
-            double distancia = double.Parse(Console.ReadLine());  
-            
-            double resultF = Physics.trabajo(fuerza,distancia);
-            Console.WriteLine($"El resultado es:{resultF}\n");
-            Console.WriteLine("Quieres calcular la fuerza nuevamente?");
-            Console.WriteLine("1.- SI           2.-NO");
-            int checker1 = int.Parse(Console.ReadLine());
-            if(checker1 == 1){
-                continue;
-            }else{
-                iterador = false;
-            }
-        }
-
-    }else if ( result == 3){
-
-        bool iterador = true;
-        while(iterador){
-            Console.WriteLine("Vamos a calcular el trabajo!");
-            Console.WriteLine("Ingrese la fuerza:");
-            double fuerza = double.Parse(Console.ReadLine());
-            Console.WriteLine("Ingres la distancia: ");
+            Console.WriteLine("Ingrese la distancia: ");
             double distancia = double.Parse(Console.ReadLine());  
             
             double resultF = Physics.trabajo(fuerza,distancia);
@@ -78,7 +57,29 @@ while(checker){
             if(checker1 == 1){
                 continue;
             }else{
-                iterador = false;
+                iterador2 = false;
+            }
+        }
+
+    }else if ( result == 3){
+
+        bool iterador3 = true;
+        while(iterador3){
+            Console.WriteLine("Vamos a calcular la energia cinetica!");
+            Console.WriteLine("Ingrese la masa:");
+            double masa = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la velocidad: ");
+            double velocidad = double.Parse(Console.ReadLine());  
+            
+            double resultF = Physics.energia_cinetica(masa,velocidad);
+            Console.WriteLine($"La energia cinetica es:{resultF}\n");
+            Console.WriteLine("Quieres calcular el trabajo nuevamente?");
+            Console.WriteLine("1.- SI           2.-NO");
+            int checker1 = int.Parse(Console.ReadLine());
+            if(checker1 == 1){
+                continue;
+            }else{
+                iterador3 = false;
             }
         }
     }
